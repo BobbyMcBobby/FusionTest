625dd4ef29ddd2469699291ebfc89bb1257ccb86
BobbyMcBobby <107490752+BobbyMcBobby@users.noreply.github.com>
2022-07-07 09:53:55 -0500

    cube
    cube

---
+++ FusionTest01/Assets/Scripts/Cube.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private OVRCameraRig camera;
+    private GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cube.transform.position = camera.centerEyeAnchor.transform.position;
+    }
+}
